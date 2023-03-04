4c54055b49e431504fa5f2b8beca3b17ecd14dce
Prasanth <124242229+Rudes-20@users.noreply.github.com>
2023-03-04 15:03:26 +0530

    abc.py

--- abc.py
+++ abc.py
@@ -1,3 +1,6 @@
+f = input("Enter a filename: ")
+e = f.split(".")[-1]
+print("The extension of the file is:", e)
 r = float(input("Enter radius of circle: "))
 a = 3.14159 * r * r
 print("Area of circle =")
